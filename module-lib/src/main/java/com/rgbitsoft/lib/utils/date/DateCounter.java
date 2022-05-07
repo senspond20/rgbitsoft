f48a563082b83559ca30626c187b6e600fd6d874
senspond20 <senspond@gmail.com>
2022-05-08 01:37:28 +0900

    DateUtils

---
+++ module-lib/src/main/java/com/rgbitsoft/lib/utils/date/DateCounter.java
@@ -0,0 +1,37 @@
+package com.rgbitsoft.lib.utils.date;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 시작 로컬날짜부터 endCount까지 날짜를 계산해 가져오는 클래스
+ */
+public class DateCounter {
+
+    private LocalDate startDate;
+    private int endCount;
+
+    private List<DateConvert> list;
+
+    public DateCounter(LocalDate startDate, int endCount){
+
+        int year = startDate.getYear();
+        int month = startDate.getMonthValue();
+        int date = startDate.getDayOfMonth();
+
+        list = new ArrayList<>();
+        for (int idx = 0; idx <= endCount; idx++){
+            DateConvert dateConvert = new DateConvert(year, month, date + idx);
+            list.add(dateConvert);
+        }
+    }
+
+    public int getEndCount() {
+        return endCount;
+    }
+
+    public List<DateConvert> getList() {
+        return list;
+    }
+}

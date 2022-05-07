afba4ad305471edafbd2b3ba549ee8e535a14bb3
senspond20 <senspond@gmail.com>
2022-05-07 17:45:50 +0900

    multi module setting - data

---
+++ module-security/src/main/java/com/rgbitsoft/auth/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.rgbitsoft.auth;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class SecurityConfig {
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+}

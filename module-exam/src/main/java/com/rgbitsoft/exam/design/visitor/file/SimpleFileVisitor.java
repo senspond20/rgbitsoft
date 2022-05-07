fa2a377559f2e6101e4fdf51e96ab91e5b3132ec
senspond20 <senspond@gmail.com>
2022-05-07 22:19:44 +0900

    Simple FileVisitor

--- module-exam/src/main/java/com/rgbitsoft/exam/design/visitor/file/SimpleFileVisitor.java
+++ module-exam/src/main/java/com/rgbitsoft/exam/design/visitor/file/SimpleFileVisitor.java
@@ -24,25 +24,38 @@ public class SimpleFileVisitor implements FileVisitor<Path> {
         return fileList;
     }
 
+    // 1. 디렉터리를 처리하기 전
     @Override
     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
         logger.debug("preVisit : {} ", dir.toString());
         return FileVisitResult.CONTINUE;
     }
 
+    // 2. 파일을 만날 때
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         logger.debug("visitFile : {} ", file.toString());
         fileList.add(file.toFile());
-        return FileVisitResult.CONTINUE;
+//        return FileVisitResult.CONTINUE;
+
+        String fileName = file.getFileName().toString();
+        if( fileName.equals("SocketAdapter.java")){
+            System.out.println( file.getParent() + " 경로에 " + fileName + "파일이 있습니다." );
+
+            return FileVisitResult.TERMINATE;
+        }else{
+            return FileVisitResult.CONTINUE;
+        }
     }
 
+    // 3. visitFile 메서드에서 예외가 일어날 때
     @Override
     public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
         logger.debug("visitFileFailed : {} ", file.toString());
         return FileVisitResult.CONTINUE;
     }
 
+    // 4. 디렉터리를 처리한 후
     @Override
     public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
         logger.debug("preVisit : {} ", dir.toString());

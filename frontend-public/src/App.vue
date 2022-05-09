f83ab7482c17e5ab74433a8bb7f87e7a85a2f4a3
senspond20 <senspond@gmail.com>
2022-05-09 23:44:41 +0900

    frontend-public vue

---
+++ frontend-public/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Logos></Logos>
+    <h1>Vue.js + Spring Boot SSR</h1>
+    <main>
+      <router-view></router-view>
+    </main>
+    <router-link to="/">Home</router-link>
+    <router-link to="/blog">Blog</router-link>
+    <div>d</div>
+  </div>
+</template>
+<script>
+import Logos from './components/Logos.vue'
+
+export default {
+  components: {
+    Logos
+  },
+}
+</script>
+
+<style>
+html, body {
+  width: 100%;
+  height: 100vh;
+}
+#app {
+  margin: auto;
+  text-align: center;
+}
+</style>
+
